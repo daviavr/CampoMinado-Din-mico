dd7dcb31f244055a8e9e2cf179bf15b83f0e171b
Davi Reis <daviavr@hotmail.com>
2021-10-23 22:18:06 -0300

    Logica Completa - Sem timer

---
+++ scripts/criacao_tabuleiro.js
@@ -0,0 +1,111 @@
+let tam_linhas, tam_colunas;
+let posicoes_bombas = [];
+let num_bombas = 15; //NUMERO ARBITRARIO PARA TESTE
+// let nome_jogador = "Joaquim" //NOME ARBITRARIO PARA TESTE
+// let modalidade = "Classico" // ARBITRARIA PARA TESTE
+
+function gera_linha() {
+    let jogo = document.getElementById("jogo");
+
+    let cria_linha = document.createElement("div");
+    cria_linha.classList.add("linha");
+    jogo.appendChild(cria_linha);
+}
+
+function gera_quadrado(cor, linha, coluna) {
+    let elemento_linha = document.getElementsByClassName("linha");
+
+    let cria_quadrado = document.createElement("button");
+    cria_quadrado.classList.add("quadrado");
+    cria_quadrado.classList.add(cor);
+    cria_quadrado.setAttribute("onclick", "pressiona_quadrado(" + linha + "," + coluna + ")");
+    cria_quadrado.setAttribute("oncontextmenu", "botao_direito(" + linha + "," + coluna + ")");
+    cria_quadrado.addEventListener('contextmenu', event => event.preventDefault());
+
+    elemento_linha[linha].appendChild(cria_quadrado);
+}
+
+function define_tamanho() {
+    let linha = document.forms["tabuleiro"]["linha"].value;
+    let coluna = document.forms["tabuleiro"]["coluna"].value;
+    linha = Number(linha);
+    coluna = Number(coluna);
+     //falta criar o campo para o usuario escolher o numero de bombas!!
+
+    cria_tabuleiro(linha,coluna);
+
+
+    return false;
+}
+
+function get_random_int(min, max) { //retorna um inteiro entre min e max, sem incluir max
+    let random = Math.random();
+    let num = Math.floor(random * (max - min)) + min;
+    return num;
+}
+
+function cria_tabuleiro(linhas,colunas) {
+    tam_linhas = linhas;
+    tam_colunas = colunas;
+    limpa_tabuleiro();
+
+    let cor = "";
+    let cor_inicial = 0;
+
+
+    for(let i = 0; i < linhas; i++){
+        gera_linha()
+
+        for(let n = cor_inicial; n < colunas; n++){ 
+            
+            cor = (n%2 ? "azul" : "roxo");
+
+            let coluna = n - cor_inicial;
+            gera_quadrado(cor, i, coluna)  ;          
+        }
+
+        colunas = (cor_inicial == 0 ? colunas + 1 : colunas - 1);
+        cor_inicial = (cor_inicial == 0 ? 1 : 0);
+    }
+
+    define_bombas();
+}
+
+function verifica_se_ha_bomba (num) {
+    let bomba_ja_existe = posicoes_bombas.find(element => element == num);
+
+    bomba_ja_existe = (bomba_ja_existe == undefined ? false : true);
+
+    return bomba_ja_existe;
+}
+
+function define_bombas(){
+    for(let i=0; i<num_bombas; i++){
+
+        do {
+            random_num =  get_random_int(0, (tam_colunas * tam_linhas));
+            bomba_ja_existe = verifica_se_ha_bomba(random_num);
+        }while(bomba_ja_existe)
+
+        posicoes_bombas.push(random_num);
+
+    }
+    // console.log(posicoes_bombas);
+
+}
+
+
+function limpa_tabuleiro() {
+    let acha_linha = document.getElementsByClassName("linha");
+    // let acha_quadrado = document.getElementsByClassName("quadrado");
+    
+    // let size_quadrados = acha_quadrado.length;
+    let size_linhas = acha_linha.length;
+    
+    for(let i = 0; i < size_linhas; i++)
+    {
+        acha_linha[0].remove();
+    }
+
+    posicoes_bombas = [];
+}
---
+++ scripts/logica_jogo.js
@@ -0,0 +1,243 @@
+function get_quadrado(linha, coluna) {
+    posicao = (linha *= tam_colunas) + coluna;
+    let quadrado = document.getElementsByClassName("quadrado")[posicao];
+    return quadrado;
+}
+
+function get_posicao_absoluta(linha, coluna) {
+    return ((linha * tam_colunas) + coluna);
+}
+
+function check_child_with_type(parent,type) {
+    let childs = parent.childNodes
+    console.log(childs.length)
+
+    for(let i = 0; i < childs.length; i++){
+        if(childs[i].nodeType == type){
+            // console.log(childs[i])
+            return [true, childs[i]]
+        }
+    }
+
+    return[false, null]
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function mostra_conteudo_quadrado(linha,coluna){
+    let coordenadas = get_coordenadas(linha,coluna);
+    let qtde_bombas = qtde_bombas_ao_redor(coordenadas);
+    let quadrado = get_quadrado(linha,coluna);
+    
+
+    
+    if(qtde_bombas != 0){
+        quadrado.innerHTML = qtde_bombas;
+        quadrado.classList.add("aberto");
+    }
+    else{
+        quadrado.innerHTML = "";
+        quadrado.classList.add("aberto");
+    }
+}
+
+async function mostra_bombas(perdeu) {
+    let quadrados = document.getElementsByClassName("quadrado");
+
+    if(!quadrados[posicoes_bombas[0]].hasChildNodes()){
+        for (let i = 0; i < num_bombas; i++) {
+            let pos = posicoes_bombas[i];
+            let imagem = document.createElement("img");
+            imagem.classList.add("bomba");
+            imagem.setAttribute("src", "../img/bomba.png");
+            quadrados[pos].appendChild(imagem);
+            // console.log(quadrados[pos]);
+        }
+    }
+
+    // if(!perdeu)
+    //     await sleep(10000);
+    //     esconde_bombas();
+}
+
+
+function esconde_bombas() {
+    let bombas = document.getElementsByClassName("bomba");
+
+    for (let i = 0; i < num_bombas; i++) {
+        bombas[0].remove();
+    }
+}
+
+function verifica_se_ha_bomba (num) {
+    let bomba_ja_existe = posicoes_bombas.find(element => element == num);
+
+    bomba_ja_existe = (bomba_ja_existe == undefined ? false : true);
+
+    return bomba_ja_existe;
+}
+
+function get_coordenadas(linha_central,coluna_central) {
+    let linha_inicial = linha_central - 1, coluna_inicial = coluna_central - 1;
+    let linha_final = linha_central + 1, coluna_final = coluna_central + 1;
+    // console.log("Depois: " + linha_inicial + " , " + coluna_inicial + " , " + linha_final + " , " + coluna_final)
+
+
+    linha_inicial = (linha_inicial < 0 ? linha_inicial = 0 : linha_inicial);
+    coluna_inicial = (coluna_inicial < 0 ? coluna_inicial = 0 : coluna_inicial);
+
+    linha_final = (linha_final >= tam_linhas ? (linha_final = tam_linhas - 1) : linha_final);
+    coluna_final = (coluna_final >= tam_colunas ? (coluna_final = tam_colunas - 1) : coluna_final);
+
+    return {
+        linha_inicial,
+        coluna_inicial,
+        linha_final,
+        coluna_final,
+        linha_central,
+        coluna_central
+    };
+}
+
+function qtde_bombas_ao_redor(coordenadas){
+    let ha_bomba, bombas_ao_redor = 0
+
+    let { linha_inicial, coluna_inicial, linha_final, 
+        coluna_final, linha_central, coluna_central } = coordenadas;
+
+    for (let i = linha_inicial; i <= linha_final; i++) {
+        for (let n = coluna_inicial; n <= coluna_final; n++) {
+            posicao_absoluta = get_posicao_absoluta(i, n);
+            ha_bomba = verifica_se_ha_bomba(posicao_absoluta);
+            if(ha_bomba){
+                if (i != linha_central || n != coluna_central) {
+                    bombas_ao_redor++;
+                }
+            }
+        }
+    }
+    return bombas_ao_redor
+}
+
+async function abre_posicoes(coordenadas) {
+    // console.log(ha_bombas)
+
+    let { linha_inicial, coluna_inicial, linha_final, 
+        coluna_final, linha_central, coluna_central } = coordenadas;
+    
+    let ha_bombas = qtde_bombas_ao_redor(coordenadas)
+
+    if(ha_bombas == 0){
+        for (let i = linha_inicial; i <= linha_final; i++) {
+            for (let n = coluna_inicial; n <= coluna_final; n++) {
+                if (i != linha_central || n != coluna_central) {
+                    let quadrado = get_quadrado(i,n)
+                    if(quadrado.classList.contains("aberto") == false){
+                        let novas_coordenadas = get_coordenadas(i,n);
+                        mostra_conteudo_quadrado(i,n);
+                        // console.log(i + "," + n)
+                        await sleep(30);
+                        abre_posicoes(novas_coordenadas);
+                    }
+                }
+            }
+        }
+    }
+    else {
+        return mostra_conteudo_quadrado(linha_central,coluna_central);
+    }
+
+}
+
+
+async function pressiona_quadrado(linha, coluna) {
+    let posicao_absoluta = get_posicao_absoluta(linha, coluna);
+    let ha_bomba = verifica_se_ha_bomba(posicao_absoluta);
+    let coordenadas = get_coordenadas(linha,coluna);
+    // console.log(coordenadas);
+    // console.log("teste")
+
+
+    if (ha_bomba) {
+        await mostra_bombas(true);
+        alert("Você perdeu!");
+        // let jogo = 
+        // new Historico(nome_jogador, `${tam_linhas}, ${tam_colunas}`, num_bombas, modalidade, tempo, "Derrota", new Date);
+    }
+    else {
+        mostra_conteudo_quadrado(linha, coluna);
+        await abre_posicoes(coordenadas);
+        if(jogador_venceu())
+        {
+            alert("Você Ganhou!");
+        }
+    }
+}
+
+function botao_direito(linha,coluna){
+    let quadrado = get_quadrado(linha,coluna)
+    let texto = document.createTextNode("?")
+
+    
+    if(!quadrado.classList.contains("aberto")) {
+        let nodes_quadrado = check_child_with_type(quadrado, 3)
+        if (nodes_quadrado[0] == true) {
+            nodes_quadrado[1].remove()
+        }
+        else{
+            quadrado.appendChild(texto);
+            // console.log(nodes_quadrado[0].nodeType("Text"))
+        }
+    }
+
+    // if(quadrado.innerHTML === "?"){
+    //     quadrado.innerHTML = ""
+    // }
+
+    return false;
+}
+
+function jogador_venceu(){
+    let tam_quadrados = document.getElementsByClassName("aberto").length;
+    let num_posicoes = (tam_linhas * tam_colunas) - num_bombas;
+
+    let ganhou = (tam_quadrados == num_posicoes ? true : false);
+    
+    return ganhou;
+
+}
+
+// function limpa_tabuleiro() {
+//     let acha_linha = document.getElementsByClassName("linha");
+//     let acha_quadrado = document.getElementsByClassName("quadrado");
+    
+//     let size_quadrados = acha_quadrado.length;
+//     let size_linhas = acha_linha.length;
+    
+//     for(let i = 0; i < size_quadrados; i++ ){
+//         acha_quadrado[0].remove();
+//     }
+
+//     for(let i = 0; i < size_linhas; i++)
+//     {
+//         acha_linha[0].remove();
+//     }
+
+//     posicoes_bombas = [];
+// }
+
+
+// class Historico{
+
+//     constructor(nome_jogador, dimensoes, num_bombas, modalidade, tempo_total, resultado, data_hora){
+//         this.nome_jogador= nome_jogador
+//         this.dimensoes = dimensoes
+//         this.num_bombas = num_bombas
+//         this.modalidade = modalidade
+//         this.tempo_total = tempo_total
+//         this.resultado = resultado
+//         this.data_hora = data_hora
+//     }
+// }
